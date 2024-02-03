5dc2cb5da654bd9a0133c9961809d1ce567cfd26
MAYANK <91362416+MAYANK20293@users.noreply.github.com>
2024-02-04 00:56:34 +0530

    Time: 47 ms (26.83%), Space: 11.2 MB (84.54%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i=0;
+        int j=1;
+        while(s.size()>j)
+        {
+            if(s[i]==s[j])
+            {
+                s.erase(i,2);
+                
+                i=max(0,i-1);
+                j=max(1,j-1);
+            }
+            else{
+                i++,j++;
+            }
+        }
+        
+        return s;
+    }
+};
